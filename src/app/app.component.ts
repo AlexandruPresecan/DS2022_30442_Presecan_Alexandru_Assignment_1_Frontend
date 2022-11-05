c04d124c773f8c11c44b77238f7df393d32e00ab
AlexandruPresecan <alexandru.presecan@gmail.com>
2022-11-05 12:13:40 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DS2022_30442_Presecan_Alexandru_Assignment_1_Frontend';
+}
