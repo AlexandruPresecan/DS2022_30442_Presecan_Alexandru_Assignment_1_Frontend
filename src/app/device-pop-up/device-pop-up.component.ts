ef86f8a3cf561e896da119dd7044c885d9a7be5b
AlexandruPresecan <alexandru.presecan@gmail.com>
2022-12-14 12:18:04 +0200

    websockets

--- src/app/device-pop-up/device-pop-up.component.ts
+++ src/app/device-pop-up/device-pop-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from 'src/environments/environment';
 import { Device } from 'src/models/device/device';
 
 @Component({
@@ -12,6 +15,29 @@ export class DevicePopUp {
 
   device?: Device;
 
+  constructor(cookieService: CookieService) {
+
+    if (!cookieService.get("token"))
+      return;
+
+    const hubConnection = new HubConnectionBuilder()
+    .withUrl(environment.apiUrl + '/notify', {
+      accessTokenFactory: function () {
+        return cookieService.get("token");
+      }
+    })
+    .build();
+
+    hubConnection.on('notify', (data) => {
+      this.openPopUp(data);
+    });
+
+    hubConnection
+    .start()
+    .then(() => console.log('Connection started'))
+    .catch(err => console.log('Error while starting connection: ' + err))
+  }
+  
   openPopUp(device: Device): void {
     this.device = device;
     this.popup.nativeElement.style.display = "block";
